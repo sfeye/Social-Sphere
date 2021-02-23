aa30ebf115d017180ea155653aa64b366c26bd0e
Samuel Feye <samfeye@gmail.com>
2021-02-23 12:30:38 -0600

    Delete

--- strapi-sphere/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'bd97600152411cc2ca8943399678470f'),
-    },
-  },
-});
--- strapi-sphere/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '23085029-96a2-4e79-8b92-c837e371c7f1'
-};
